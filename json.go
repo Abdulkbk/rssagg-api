8c55d3f38210e29ac42c2c81fd811383dffd80fc
Abdullahi Yunus <abdoollahikbk@gmail.com>
2024-05-26 19:17:49 +0200

    setup: rss aggregator setup

---
+++ json.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	if code > 499 {
+		log.Println("Responding with 5XX error: ", message)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errResponse{
+		Error: message,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("Failed to marshal JSON response: ", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
